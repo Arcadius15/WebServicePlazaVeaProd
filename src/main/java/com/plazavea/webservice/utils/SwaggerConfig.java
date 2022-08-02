974b84cfe3a8a018964156ef11489142402ebe04
Diego Vicente <diegovic99@hotmail.com>
2022-08-01 22:01:38 -0500

    Cambio en Scope Swagger

--- src/main/java/com/plazavea/webservice/utils/SwaggerConfig.java
+++ src/main/java/com/plazavea/webservice/utils/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.plazavea.webservice.utils;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +11,11 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -28,6 +32,7 @@ public class SwaggerConfig {
 				.build()
 				.apiInfo(getApiInfo())
                 .securitySchemes(Arrays.asList(apiKey()))
+				.securityContexts(Arrays.asList(securityContext()))
 				;
 	}
 	
@@ -47,4 +52,16 @@ public class SwaggerConfig {
     private ApiKey apiKey() {
         return new ApiKey("jwtToken", "Authorization", "header");
     }
+
+	private SecurityContext securityContext(){
+		return SecurityContext.builder().securityReferences(defaultAuth()).forPaths(PathSelectors.any()).build();
+	}
+
+	List<SecurityReference> defaultAuth(){
+		AuthorizationScope authorizationScope
+            = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return Arrays.asList(new SecurityReference("jwtToken", authorizationScopes));
+	}
 }
